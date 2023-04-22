f77047e3840f5dc3be94155dcab2ebdd5cbf3bb0
Nicola Cuomo <nicola.cuomo@relatech.com>
2023-04-22 10:23:33 +0200

    move into monorepo

---
+++ server/prisma/migrations/20230416205137_move_to_activity/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Todo` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Todo";
+
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "text" TEXT NOT NULL,
+
+    CONSTRAINT "Activity_pkey" PRIMARY KEY ("id")
+);
---
+++ server/src/index.ts
@@ -0,0 +1,74 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.raw({ type: "application/vnd.custom-type" }));
+app.use(express.text({ type: "text/html" }));
+
+app.get("/activity", async (req, res) => {
+  const activities = await prisma.activity.findMany({
+    orderBy: { createdAt: "desc" },
+  });
+
+  res.json(activities);
+});
+
+app.post("/activity", async (req, res) => {
+  const todo = await prisma.activity.create({
+    data: {
+      text: req.body.text ?? "Empty todo",
+    },
+  });
+
+  return res.json(todo);
+});
+
+app.get("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  const todo = await prisma.activity.findUnique({
+    where: { id },
+  });
+
+  return res.json(todo);
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  const todo = await prisma.activity.update({
+    where: { id },
+    data: req.body,
+  });
+
+  return res.json(todo);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  await prisma.activity.delete({
+    where: { id },
+  });
+
+  return res.send({ status: "ok" });
+});
+
+app.get("/", async (req, res) => {
+  res.send(
+    `
+  <h1>Todo REST API</h1>
+  <h2>Available Routes</h2>
+  <pre>
+    GET, POST /todos
+    GET, PUT, DELETE /todos/:id
+  </pre>
+  `.trim()
+  );
+});
+
+app.listen(Number(port), "0.0.0.0", () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
